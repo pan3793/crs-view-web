26d81d69f96a78c6f61117716ab0851dc7929075
pancheng <379377944@qq.com>
2017-12-11 23:51:56 +0800

    修改topbar菜单

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,19 +1,21 @@
 <template>
 <div>
   <el-row type="flex" align="middle" justify="space-around">
-    <el-col :span="5" style="font-size: 24px">
+    <el-col :span="4" style="font-size: 24px">
         <i class="el-icon-edit"/>课程资源系统
     </el-col>
-    <el-col :span="9">
+    <el-col :span="11">
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" backgroud-color="blueviolet">
-        <el-menu-item index="1">微课堂</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">课程分类</template>
-            <el-menu-item index="2-1">计算机</el-menu-item>
-            <el-menu-item index="2-2">经济学</el-menu-item>
-            <el-menu-item index="2-3">数理</el-menu-item>
-          </el-submenu>
-        <el-menu-item index="3">交流论坛</el-menu-item>
+        <el-menu-item index="1">精品课程</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">分类</template>
+          <el-menu-item index="2-1">计算机</el-menu-item>
+          <el-menu-item index="2-2">经济学</el-menu-item>
+          <el-menu-item index="2-3">数理</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3">作业</el-menu-item>
+        <el-menu-item index="4">在线考试</el-menu-item>  
+        <el-menu-item index="5">交流论坛</el-menu-item>
       </el-menu>
     </el-col>
     <el-col :span="5">
@@ -21,9 +23,9 @@
         <el-button slot="append" type="primary" icon="el-icon-search"></el-button>
       </el-input>
     </el-col>
-    <el-col :span="5">
-      <el-button type="primary">登录</el-button>
-      <el-button type="warning">注册</el-button>
+    <el-col :span="4">
+      <el-button type="primary" size="medium">登录</el-button>
+      <el-button type="warning" size="medium">注册</el-button>
     </el-col>
   </el-row>
 </div>
@@ -34,7 +36,6 @@
     data () {
       return {
         activeIndex: '1',
-        activeIndex2: '1',
         input: ''
       }
     },

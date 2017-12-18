91f84174befd7fc4e9fbd90581f1ffeb7547751e
pancheng <379377944@qq.com>
2017-12-19 00:07:40 +0800

    menu动态构建

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,34 +1,54 @@
 <template>
-<div>
-  <el-row type="flex" align="middle" justify="space-around">
-    <el-col :span="4" style="font-size: 24px">
-        <i class="el-icon-edit"/>课程资源系统
-    </el-col>
-    <el-col :span="11">
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" backgroud-color="blueviolet">
-        <el-menu-item index="1">精品课程</el-menu-item>
-        <el-submenu index="2">
-          <template slot="title">分类</template>
-          <el-menu-item index="2-1">计算机</el-menu-item>
-          <el-menu-item index="2-2">经济学</el-menu-item>
-          <el-menu-item index="2-3">数理</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="3">作业</el-menu-item>
-        <el-menu-item index="4">在线考试</el-menu-item>  
-        <el-menu-item index="5">交流论坛</el-menu-item>
-      </el-menu>
-    </el-col>
-    <el-col :span="5">
-      <el-input v-model="input" placeholder="请输入内容">
-        <el-button slot="append" type="primary" icon="el-icon-search"></el-button>
-      </el-input>
-    </el-col>
-    <el-col :span="4">
-      <el-button type="primary" size="medium">登录</el-button>
-      <el-button type="warning" size="medium">注册</el-button>
-    </el-col>
-  </el-row>
-</div>
+  <div>
+    <el-row type="flex" align="middle" justify="space-around">
+      
+      <el-col :span="4" style="font-size: 24px">
+          <i class="el-icon-edit"/>课程资源系统
+      </el-col>
+      
+      <el-col :span="11">
+        <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect" backgroud-color="blueviolet">
+          
+          <template v-for="item in menu">
+  
+            <el-submenu 
+              v-if="item.children"
+              :key="item.index"
+              :index="item.url">
+              <template slot="title">{{item.title}}</template>
+              <el-menu-item 
+                v-for="item in item.children"
+                :key="item.index"
+                :index="item.url">
+                {{item.title}}
+              </el-menu-item>
+            </el-submenu>
+
+            <el-menu-item 
+              v-else
+              :key="item.index"
+              :index="item.url">
+              {{item.title}}
+            </el-menu-item>
+
+          </template>
+
+        </el-menu>
+      </el-col>
+
+      <el-col :span="5">
+        <el-input v-model="input" placeholder="请输入内容">
+          <el-button slot="append" type="primary" icon="el-icon-search"></el-button>
+        </el-input>
+      </el-col>
+
+      <el-col :span="4">
+        <el-button type="primary" size="medium">登录</el-button>
+        <el-button type="warning" size="medium">注册</el-button>
+      </el-col>
+
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -36,7 +56,45 @@
     data () {
       return {
         activeIndex: '1',
-        input: ''
+        input: '',
+        menu: [
+          {
+            index: '1',
+            title: '精品课程',
+            url: '/1'
+          },
+          {
+            index: '2',
+            title: '分类',
+            children: [
+              {
+                index: '2-1',
+                title: '计算机',
+                url: '/2-1'
+              },
+              {
+                index: '2-2',
+                title: '经济学',
+                url: '/2-2'
+              }
+            ]
+          },
+          {
+            index: '3',
+            title: '作业',
+            url: '/3'
+          },
+          {
+            index: '4',
+            title: '在线考试',
+            url: '/4'
+          },
+          {
+            index: '5',
+            title: '交流论坛',
+            url: '/5'
+          }
+        ]
       }
     },
     methods: {
